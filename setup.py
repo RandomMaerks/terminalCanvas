00b122c2048deae49d8763ca5ccb5501c9c1de7c
RandomMaerks <rmforbusiness@gmail.com>
2026-05-27 19:49:14 +0700

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as file:
 
 setuptools.setup(
     name = 'terminalCanvas',
-    version = '0.1.3',
+    version = '0.1.4',
 
     description = 'A module for creating a canvas and displaying text-based graphics in the terminal',
     long_description = readme,
