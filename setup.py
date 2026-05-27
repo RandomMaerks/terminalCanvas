b1fac1018cffbcfd690ce67b432957ff38660982
RandomMaerks <rmforbusiness@gmail.com>
2026-05-27 16:50:31 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 import setuptools
 
+with open('README.md') as file:
+    readme = file.read()
+
 setuptools.setup(
     name = 'terminalCanvas',
     version = '0.1.0',
+
     description = 'A module for creating a canvas and displaying text-based graphics in the terminal',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+
     author = 'RandomMaerks',
     author_email = 'rmforbusiness@gmail.com',
     url = 'https://github.com/RandomMaerks/terminalCanvas',
+
     license = 'GPLv3',
     license_files = ['LICENSE',],
+
     packages = setuptools.find_packages(),
+
     python_requires = '>=3.10',
     install_requires = ['pillow', 'numpy',],
+    
     classifiers = [
         'Programming Language :: Python :: 3',
         'Operating System :: Microsoft :: Windows',
