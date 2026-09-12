98a267bfca75b633f6b4c03838f1d0cbc0a7d2fa
RandomMaerks <rmforbusiness@gmail.com>
2026-09-12 23:32:54 +0700

    Rewrite `Line()` to include thickness

--- terminalCanvas/objects.py
+++ terminalCanvas/objects.py
@@ -164,7 +164,7 @@ class Line(BaseObject):
             x1: int | float = 0, y1: int | float = 0,
             x2: int | float = 0, y2: int | float = 0,
             color: tuple[int, int, int, int] = (0, 0, 0, 255),
-            antialiasing: bool = False
+            thickness: int = 1,
     ) -> None:
 
         super().__init__()
@@ -172,7 +172,7 @@ class Line(BaseObject):
         self.x1, self.y1 = x1, y1
         self.x2, self.y2 = x2, y2
         self.color = color
-        self.antialiasing = antialiasing
+        self.thickness = thickness
         self._build()
 
     def _build(self):
@@ -184,60 +184,157 @@ class Line(BaseObject):
         x2 = roundInt(self.x2)
         y2 = roundInt(self.y2)
 
+        thickness = max(roundInt(self.thickness), 1)
+
         color = self.color
-     
-        dx = abs(x2 - x1)
-        sx = 1 if x1 < x2 else -1
 
-        dy = -abs(y2 - y1)
-        sy = 1 if y1 < y2 else -1
-
-        error = dx + dy
-
-        if not self.antialiasing:            
-            while True:
-                self._add([x1, y1, color])
-
-                if x1 == x2 and y1 == y2: break
-
-                e2 = 2 * error
-
-                if e2 >= dy:
-                    error += dy
-                    x1 += sx
-                if e2 <= dx:
-                    error += dx
-                    y1 += sy
-        
+        if thickness <= 1:
+            self._drawLineOverlap(x1, y1, x2, y2, color)
         else:
-            ed = 1 if dx - dy == 0 else (dx*dx + dy*dy) ** 0.5
-            r, g, b, *_ = color
-            while True:
-                aa = 255 - (255 * abs(error - dx - dy) / ed)
-                self._add([x1, y1, (r, g, b, aa)])
+            dy = x2 - x1
+            dx = y2 - y1
 
-                e2 = error
-                x3 = x1
+            swap = True
+            if dx < 0:
+                dx = -dx
+                sx = -1
+                swap = not swap
+            else:
+                sx = 1
 
-                if e2 * 2 >= -dx:
-                    if x1 == x2: break
-                    if e2 - dy < ed:
-                        aa = 255 - (255 * (e2 - dy) / ed)
-                        self._add([x1, y1 + sy, (r, g, b, aa)])
-                    error += dy
+            if dy < 0:
+                dy = -dy
+                sy = -1
+                swap = not swap
+            else:
+                sy = 1
+
+            dx2 = dx << 1
+            dy2 = dy << 1
+
+            offset = thickness // 2
+
+            if dx >= dy:
+                if swap:
+                    offset = (thickness - 1) - offset
+                    sy = -sy
+                else:
+                    sx = -sx
+                    
+                error = dy2 - dx
+                for i in range(offset, 0, -1):
+                    x1 -= sx
+                    x2 -= sx
+                    if error >= 0:
+                        y1 -= sy
+                        y2 -= sy
+                        error -= dx2
+                    error += dy2
+
+                self._drawLineOverlap(x1, y1, x2, y2, color)
+
+                error = dy2 - dx
+                for i in range(thickness, 1, -1):
                     x1 += sx
+                    x2 += sx
+                    overlap = "none"
+                    if error >= 0:
+                        y1 += sy
+                        y2 += sy
+                        error -= dx2
+                        overlap = "major"
+                    error += dy2
 
-                if e2 * 2 <= -dy:
-                    if y1 == y2: break
-                    if dx - e2 < ed:
-                        aa = 255 - (255 * abs(dx - e2) / ed)
-                        self._add([x3 + sx, y1, (r, g, b, aa)])
-                    error += dx
+                    self._drawLineOverlap(x1, y1, x2, y2, color, overlap)
+            else:
+                if swap:
+                    sx = -sx
+                else:
+                    offset = (thickness - 1) - offset
+                    sy = -sy
+
+                error = dx2 - dy
+                for i in range(offset, 0, -1):
+                    y1 -= sy
+                    y2 -= sy
+                    if error >= 0:
+                        x1 -= sx
+                        x2 -= sx
+                        error -= dy2
+                    error += dx2
+
+                self._drawLineOverlap(x1, y1, x2, y2, color)
+
+                error = dx2 - dy
+                for i in range(thickness, 1, -1):
                     y1 += sy
+                    y2 += sy
+                    overlap = "none"
+                    if error >= 0:
+                        x1 += sx
+                        x2 += sx
+                        error -= dy2
+                        overlap = "major"
+                    error += dx2
+
+                    self._drawLineOverlap(x1, y1, x2, y2, color, overlap)
+     
+    def _drawLineOverlap(
+            self,
+            x1: int, y1: int,
+            x2: int, y2: int,
+            color: tuple[int, int, int, int],
+            overlap: str = "none"
+    ):
+        dx = x2 - x1
+        dy = y2 - y1
+
+        if dx < 0:
+            dx = -dx
+            sx = -1
+        else:
+            sx = 1
+
+        if dy < 0:
+            dy = -dy
+            sy = -1
+        else:
+            sy = 1
+
+        dx2 = dx << 1
+        dy2 = dy << 1
+
+        self._add([x1, y1, color])
+        if dx > dy:
+            error = dy2 - dx
+            while x1 != x2:
+                x1 += sx
+                if error >= 0:
+                    if overlap == "major": self._add([x1, y1, color])
+                    y1 += sy
+                    if overlap == "minor": self._add([x1 - sx, y1, color])
+                    error -= dx2
+                error += dy2
+                self._add([x1, y1, color])
+        else:
+            error = dx2 - dy
+            while y1 != y2:
+                y1 += sy
+                if error >= 0:
+                    if overlap == "major": self._add([x1, y1, color])
+                    x1 += sx
+                    if overlap == "minor": self._add([x1, y1 - sy, color])
+                    error -= dy2
+                error += dx2
+                self._add([x1, y1, color])        
 
     def set_points(self, x1, y1, x2, y2):
         self.x1, self.y1 = x1, y1
         self.x2, self.y2 = x2, y2
+        self._build()
+
+    def set_thickness(self, thickness):
+        self.thickness = thickness
         self._build()
 
 class Triangle(BaseObject):
