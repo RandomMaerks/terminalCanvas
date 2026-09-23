7d4969d06586b65cdd0ef502d035bda0efd09a06
RandomMaerks <rmforbusiness@gmail.com>
2026-09-23 22:20:26 +0700

    Change docstring for BaseCompoundObject

--- terminalCanvas/objects/BaseCompoundObject.py
+++ terminalCanvas/objects/BaseCompoundObject.py
@@ -6,14 +6,33 @@ class BaseCompoundObject:
     Then, use `super().__init__()` to call the BaseCompoundObject `__init__` method.
     This will ensure all the required attributes are present in the child class.
 
-    All BaseObject shapes that make up the BaseCompoundObject shape should go in `self.objects`, simply by doing `self.objects.append()`.
-    Then, when drawn, the canvas will look through this list and automatically pick up all of those shapes.
+    `_build()` is a required method which calculates all the necessary shapes that make up the intended compound object.
+    You must have a `_build()` method for your object and it must have `self._empty()` at the very start.
 
-    You can simply make all the calculations for the desired shapes in `__init__(self)`,
-    or add your own methods for more nuanced computations.
-
+    All BaseObject shapes that make up the BaseCompoundObject shape should go in `self.objects`, simply by doing `self._add(object)`.
+    Their attributes depend on what you want to achieve with your compound object.
+    e.g. `self._add(Line3D(-1, 0, 0, 1, 0, 0, color=(255, 0, 0)))` adds an instance of Line3D to the object data.
+    
     For setter methods, make sure to apply the changes to **every single object in `self.objects`**.
     This is merely a reminder, but it's still important to check.
+    Additionally, set `_modified` to True for each object (e.g. `for obj in self.objects: obj._modified = True`).
+
+    Public methods:
+    - `move(x, y)`: move all objects by some x and y pixels
+    - `scale(x)`: scale all objects up by a factor of integer x
+    - `set_color((R, G, B[, A]))`: change the color of all objects (change if colors are different between objects)
+    - `collides(other)`: detect collision of self with other (other being BaseObject and its subclasses)
+    - `includes((x, y))`: detect coordinate on pixel data of self
+
+    Private methods:
+    - `_empty()`: reset object data
+    - `_add(object: BaseObject)`: add object to object data
+    - `_build()`: recalculate object data
+    
+    Attributes:
+    - `data`: a list of pixels, each of which contains coordinate and color data
+    - `_modified`: state of object, whether its attributes have been modified
+    - `_compound`: indicator for TCanvas. Do not delete or modify.
     """
 
     def __init__(self) -> None:
