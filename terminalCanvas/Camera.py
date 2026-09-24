dcac531b84cf4fb9e43d9c938614e353ee32ee44
RandomMaerks <rmforbusiness@gmail.com>
2026-09-25 00:47:37 +0700

    Add `set_position()` and `set_angle()` for Camera

--- terminalCanvas/Camera.py
+++ terminalCanvas/Camera.py
@@ -27,6 +27,12 @@ class Camera:
 
     # Camera transformation
 
+    def set_position(self, x: float = 0.0, y: float = 0.0, z: float = 0.0) -> None:
+        self.position = np.array([x, y, z], dtypes = np.float64)
+
+    def set_angle(self, ax: float = 0.0, ay: float = 0.0, az: float = 0.0) -> None:
+        self.angle = np.array([ax, ay, az], dtypes = np.float64)
+
     def move(self, x: float = 0.0, y: float = 0.0, z: float = 0.0) -> None:
         self.position += np.array([x, y, z])
 
